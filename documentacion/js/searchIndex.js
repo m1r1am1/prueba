96f653f082b3378e9f291ee18d647520e69b6ce4
m1r1am1 <133687619+m1r1am1@users.noreply.github.com>
2023-05-17 16:58:38 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020est\u00E1tica\u0020que\u0020contiene\u0020m\u00E9todos\u0020para\nrealizar\u0020operaciones\u0020b\u00E1sicas\u003A\u0020suma,\u0020resta,\nmultiplicaci\u00F3n\u0020y\u0020divisi\u00F3n.",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Asumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020que\u0020recibe\u0020dos\u0020n\u00FAmeros\u0020como\u0020par\u00E1metros\u0020y\u0020realiza\u0020la\u0020suma\u0020de\u0020los\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_sumar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Arestar\u0028\u0029",
+            "name": "restar",
+            "summary": "Funci\u00F3n\u0020que\u0020recibe\u0020dos\u0020n\u00FAmeros\u0020como\u0020par\u00E1metros\u0020y\u0020realiza\u0020la\u0020resta\u0020de\u0020los\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_restar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Amultiplicar\u0028\u0029",
+            "name": "multiplicar",
+            "summary": "Funci\u00F3n\u0020que\u0020recibe\u0020dos\u0020n\u00FAmeros\u0020como\u0020par\u00E1metros\u0020y\u0020realiza\u0020la\u0020mulltiplicaci\u00F3n\u0020de\u0020los\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_multiplicar"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Adividir\u0028\u0029",
+            "name": "dividir",
+            "summary": "Funci\u00F3n\u0020que\u0020recibe\u0020dos\u0020n\u00FAmeros\u0020como\u0020par\u00E1metros\u0020y\u0020realiza\u0020la\u0020divisi\u00F3n\u0020de\u0020los\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_dividir"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
